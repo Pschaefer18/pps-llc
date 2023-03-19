121d245137760d4ec61e50922249cce1257f25be
Pschaefer18 <paul.r.schaefer11@gmail.com>
2023-03-19 00:25:45 -0400

    commented out stripe files

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,13 +1,13 @@
-import Stripe from "stripe";
+// import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+// const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
-    if(req.method === 'POST') {
-        try {
+// export default async function handler(req, res) {
+//     if(req.method === 'POST') {
+//         try {
             
-        } catch {
-            res.status(500).json({ statusCode: 500, message: error.message })
-        }
-    }
-}
+//         } catch {
+//             res.status(500).json({ statusCode: 500, message: error.message })
+//         }
+//     }
+// }
